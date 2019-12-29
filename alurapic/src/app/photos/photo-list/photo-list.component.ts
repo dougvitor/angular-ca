23dd047f3aac940be6a6695e6c46ac829981c652
douguees <admin@example.com>
2019-12-29 12:14:59 -0300

    CA Fundamentos - Projeto Final

---
+++ alurapic/src/app/photos/photo-list/photo-list.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+
+import { ActivatedRoute } from '@angular/router';
+import { Photo } from '../photo/photo';
+import { PhotoService } from '../photo/photo.service';
+
+
+@Component({
+  selector: 'ap-photo-list',
+  templateUrl: './photo-list.component.html',
+  styleUrls: ['./photo-list.component.css']
+})
+export class PhotoListComponent implements OnInit {
+  
+  photosList: Photo[] = [];
+  filter: string = "";
+  hasMore: boolean = true;
+  currentPage: number = 1;
+  username: string = '';
+  
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private photoService: PhotoService
+    ){}
+  
+  ngOnInit(): void {
+    this.username = this.activatedRoute.snapshot.params.username;
+    this.photosList = this.activatedRoute.snapshot.data['photosResolver'];
+  }
+
+  load(){
+    this.photoService
+      .listFromUserPaginated(this.username, ++this.currentPage)
+      .subscribe(photos => {
+        this.filter = '';
+        this.photosList = this.photosList.concat(photos);
+
+        if(!photos.length){
+          this.hasMore = false;
+        }
+      });
+  }
+}
