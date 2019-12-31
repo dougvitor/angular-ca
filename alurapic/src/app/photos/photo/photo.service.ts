f60793ea7553da738b91aeb99805fe73fadef291
douguees <admin@example.com>
2019-12-31 10:53:51 -0300

    CA Fundamentos - Final do curso parte 1 (ajustes)

--- alurapic/src/app/photos/photo/photo.service.ts
+++ alurapic/src/app/photos/photo/photo.service.ts
@@ -13,13 +13,13 @@ export class PhotoService{
 
     listFromUser(username: string): Observable<Photo[]>{
         return this.http
-                .get<Photo[]>(API + '/' + username + '/photos');
+                .get<Photo[]>(`${API}/${username}/photos`);
     }
 
     listFromUserPaginated(username: string, page: number): Observable<Photo[]>{
         const params = new HttpParams()
             .append('page', page.toString());
 
-        return this.http.get<Photo[]>(API + '/' + username + '/photos', {params});
+        return this.http.get<Photo[]>(`${API}/${username}/photos`, {params});
     }
 }
