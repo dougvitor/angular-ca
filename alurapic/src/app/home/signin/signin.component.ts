daa7b889a390ea834f570254ab6b6c4d5b726a0b
douguees <admin@example.com>
2020-01-02 12:27:10 -0300

    P2-1 -  Enviando credenciais para a API

--- alurapic/src/app/home/signin/signin.component.ts
+++ alurapic/src/app/home/signin/signin.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/core/auth.service';
 
 @Component({
     templateUrl: './signin.component.html'
@@ -8,12 +9,32 @@ export class SignInComponent implements OnInit{
     
     loginForm: FormGroup;
     
-    constructor(private formBuilder: FormBuilder){ }
+    constructor(private formBuilder: FormBuilder, private authService: AuthService){ }
     
     ngOnInit(): void {
         this.loginForm = this.formBuilder.group({
             username: ['', Validators.required],
             password:['', Validators.required]
-        })
+        });
+    }
+
+    login(){
+
+        const username = this.loginForm.get('username').value;
+        const password = this.loginForm.get('password').value;
+
+        this.authService
+            .authenticate(username, password)
+            .subscribe(() => console.log('AUTENTICADO'), 
+                        err => {
+                                    console.log(err);
+
+                                    this.loginForm.reset();
+
+                                    alert('Invalid user name or password');
+
+                        }
+                    );
+
     }
 }
