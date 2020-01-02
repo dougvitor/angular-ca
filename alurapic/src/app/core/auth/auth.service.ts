dc8766f4b9d82855c130ba5e3e3d6d1d289f7b8a
douguees <admin@example.com>
2020-01-02 13:48:42 -0300

    P2-2 - Acessando o header da resposta

--- alurapic/src/app/core/auth/auth.service.ts
+++ alurapic/src/app/core/auth/auth.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 const API_URL = 'http://localhost:3000';
 
@@ -14,7 +14,17 @@ export class AuthService {
 
   authenticate(username: string, password: string){
 
-    return this.http.post(`${API_URL}/user/login`, {userName: username, password});
+    return this.http.post(
+                            `${API_URL}/user/login`,   
+                            {userName: username, password}, 
+                            { observe: 'response'}
+                      )
+                      .pipe(
+                            tap(resp => {
+                                const authToken = resp.headers.get('x-access-token');
+                                console.log(`User ${username} authenticated with token ${authToken}`);                        
+                            })
+                      );
 
   }
 }
