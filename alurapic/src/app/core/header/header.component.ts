8ed1890d52455688794bcdb6570b217c3b1ede51
douguees <admin@example.com>
2020-01-02 17:20:54 -0300

    P2-3 - Implementação do logout

--- alurapic/src/app/core/header/header.component.ts
+++ alurapic/src/app/core/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { UserService } from '../user/user.service';
 import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
 import { User } from '../user/user';
 
 @Component({
@@ -11,9 +12,15 @@ export class HeaderComponent{
 
     user$: Observable<User>;
 
-    constructor(userService: UserService){
+    constructor(private userService: UserService, 
+        private router: Router){
 
         this.user$ = userService.getUser();
+    }
+
+    logout(){
+        this.userService.logout();
+        this.router.navigate(['']);
     }
 
 }
