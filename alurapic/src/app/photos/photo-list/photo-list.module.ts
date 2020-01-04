66be038bf6953d50988e39917f71814e4372ba35
douguees <admin@example.com>
2020-01-04 00:12:31 -0300

    P3-3 -  Extraindo parametros de rotas

--- alurapic/src/app/photos/photo-list/photo-list.module.ts
+++ alurapic/src/app/photos/photo-list/photo-list.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 import { PhotoListComponent } from './photo-list.component';
 import { PhotosComponent } from './photos/photos.component';
@@ -22,6 +23,7 @@ import { DarkenOnHoverModule } from 'src/app/shared/directives/darken-on-hover/d
         CommonModule,
         PhotoModule,
         CardModule,
+        RouterModule,
         DarkenOnHoverModule
     ]
 })
